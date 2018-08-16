483015a93d0e5f078a9ea0282c31e1a565364227
tkaneda0078 <1111510078r21@gmail.com>
2018-08-17 08:58:18 +0900

    upload処理

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const multer  = require('multer');
+const upload = multer({ dest: './uploads/' });
 
-/* GET home page. */
+/**
+ * home
+ */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/**
+ * 画像認識
+ */
+router.post('/recognition_decision',  upload.single('image_model'), (req, res) => {
+  console.log(req.file);
 });
 
 module.exports = router;
